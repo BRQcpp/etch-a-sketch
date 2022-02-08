e79f119e380bf3a4d9fa2779c691886b0111f6e4
BRQ <bnowak4744@gmail.com>
2022-02-08 17:07:30 +0100

    Updated to next stage of the project

--- source.js
+++ source.js
@@ -1,43 +1,63 @@
 let container = document.querySelector('.sketch-container');
 let containerMaxSize = 700; // Max size of sketching square
 let squareSideSize = 10;
+const minsquareSideSize = 10;
 let mouseDown = false;
-let width = document.querySelector('#width').value;
-let height = document.querySelector('#height').value;
+let widthValue = document.querySelector('#width').value;
+let heightValue = document.querySelector('#height').value;
+let widthInput = document.querySelector('#width');
+let heightInput = document.querySelector('#height');
 let submitButton = document.querySelector('.resize-button');
 let pixelResizeInput = document.querySelector("#pixel-size");
-let paintbrushTool = document.querySelector('.paintbursh-tool');
-let ereaseTool = document.querySelector('.erease-tool');
+let paintbrushTool = document.querySelector('.paintbursh-selection');
+let rainbowbrushTool = document.querySelector('.rainbow-brush-selection');
+let blackbrushTool = document.querySelector('.blackbrush-increase-selection');
+let eraseTool = document.querySelector('.eraser-selecion');
+let blackColorValue = 175; 
 
+const paintburshSelected = paintbrushTool.getAttribute('id');
+const eraserSelected = eraseTool.getAttribute('id');
+const rainbowbrushSelected = rainbowbrushTool.getAttribute('id');
+const blackbrushSelected = blackbrushTool.getAttribute('id');
 
-let ereaseSelected = false;
-let paintburshSelected = true;
+let selectedTool = 0;
 
 pixelResizeInput.setAttribute('max', containerMaxSize);
 
 setContainerSize(squareSideSize, containerMaxSize);
 
-generateBoxes(width, height, squareSideSize);
+generateBoxes(widthValue, heightValue, squareSideSize);
 
 addPaintEvent();
 
+setSelected();
 
-pixelResizeInput.addEventListener('change', () => {
-    squareSideSize = pixelResizeInput.value;
-    setContainerSize(squareSideSize, containerMaxSize);
-    let width = document.querySelector('#width').value;
-    let height = document.querySelector('#height').value;
-    generateBoxes(width, height, squareSideSize);
+paintbrushTool.addEventListener('click', () => 
+{
+    unsetSelected();
+    selectedTool = +paintburshSelected;
+    setSelected();
 });
 
-ereaseTool.addEventListener('click', () => {
-    ereaseSelected = true;
-    paintburshSelected = false;
+rainbowbrushTool.addEventListener('click', () => 
+{
+    unsetSelected();
+    selectedTool = +rainbowbrushSelected;
+    setSelected();
 });
 
-paintbrushTool.addEventListener('click', () => {
-    ereaseSelected = false;
-    paintburshSelected = true;
+blackbrushTool.addEventListener('click', () => 
+{
+    unsetSelected();
+    selectedTool = +blackbrushSelected;
+    setSelected();
+});
+
+eraseTool.addEventListener('click', () => 
+{
+    unsetSelected();
+    selectedTool = +eraserSelected;
+    setSelected();
 });
 
 container.addEventListener('mousedown', () => 
@@ -57,10 +77,38 @@ container.addEventListener('mouseleave', () =>
 
 submitButton.addEventListener('click', () =>
 {
-    let width = document.querySelector('#width').value;
-    let height = document.querySelector('#height').value;
-    generateBoxes(width, height, squareSideSize);
+    squareSideSize = pixelResizeInput.value;
+    
+    if(squareSideSize < minsquareSideSize || squareSideSize > containerMaxSize)
+        alert(`Pixel size ranges from ${minsquareSideSize} to ${containerMaxSize}`);
+    else 
+    {
+        setContainerSize(squareSideSize, containerMaxSize);
+        generateBoxes(widthValue, heightValue, squareSideSize);
+    }
 });
+
+function setSelected() 
+{
+    switch(selectedTool)
+    {
+        case 0 : document.getElementById('0').classList.add('tool-selected'); break;
+        case 1 : document.getElementById('1').classList.add('tool-selected'); break;
+        case 2 : document.getElementById('2').classList.add('tool-selected'); break;
+        case 3 : document.getElementById('3').classList.add('tool-selected'); break;
+    }
+}
+
+function unsetSelected() 
+{
+    switch(selectedTool)
+    {
+        case 0 : document.getElementById('0').classList.remove('tool-selected'); break;
+        case 1 : document.getElementById('1').classList.remove('tool-selected'); break;
+        case 2 : document.getElementById('2').classList.remove('tool-selected'); break;
+        case 3 : document.getElementById('3').classList.remove('tool-selected'); break;
+    }
+}
 
 function setPixelSize(squareSideSize)
 {
@@ -72,32 +120,44 @@ function setPixelSize(squareSideSize)
     });
 }
 
+function getRandomNumber(min, max)
+{
+    return Math.random() * (+max +1 - +min) + +min;
+}
+
+function setBrushColor(block)
+{
+    switch(selectedTool)
+    {
+        case 0 : block.style.setProperty("background-color", `${document.querySelector('.paintbrushColor-selector').value}`); break;
+        case 1 : block.style.setProperty("background-color", `rgba(${getRandomNumber(0,255)}, ${getRandomNumber(0,255)}, ${getRandomNumber(0,255)})`); break;
+        case 2 : 
+        {
+            block.style.setProperty("background-color", `rgb(${blackColorValue}, ${blackColorValue}, ${blackColorValue})`);
+            blackColorValue -= 5;
+            // /console.log(blackColorValue);
+            if(blackColorValue <= 0)
+                blackColorValue = 175;
+        }break;
+        case 3 : block.style.setProperty("background-color", 'white'); break;
+    }
+}
+
 function addPaintEvent()
 {
     let blocks = document.querySelectorAll('.test');
     blocks.forEach( block =>
     {
-
             block.addEventListener('mouseover', () =>
             {
                 if(mouseDown)
-                {
-                    if(paintburshSelected)
-                        block.style.setProperty("background-color", `${document.querySelector('.paintbrushColor-selector').value}`);
-                    else
-                        block.style.setProperty("background-color", 'white');
-                }   
-
+                    setBrushColor(block);
             } );
 
             block.addEventListener('click', () =>
             {
                     mouseDown = false;
-                    
-                    if(paintburshSelected)
-                        block.style.setProperty("background-color", `${document.querySelector('.paintbrushColor-selector').value}`);
-                    else
-                        block.style.setProperty("background-color", 'white');
+                    setBrushColor(block);
             } );
     });
 }
@@ -106,19 +166,34 @@ function addPaintEvent()
 
 function setContainerSize(squareSideSize, containerMaxSize)
 {
-    let widthValue = document.querySelector('.sketch-container').style.getPropertyValue("width");
-    let heightValue = document.querySelector('.sketch-container').style.getPropertyValue("height");
-    widthValue = squareSideSize * Math.floor(containerMaxSize/squareSideSize);
-    heightValue = squareSideSize * Math.floor(containerMaxSize/squareSideSize);
+    widthValue = squareSideSize * Math.floor(document.querySelector('#width').value/squareSideSize);
+    heightValue = squareSideSize * Math.floor(document.querySelector('#height').value/squareSideSize);
     document.querySelector('#width').value = widthValue;
     document.querySelector('#height').value = heightValue;
-    document.querySelector('#width').setAttribute('step', squareSideSize);
-    document.querySelector('#height').setAttribute('step', squareSideSize);
+    container.style.setProperty("max-width", `${widthValue}px`);
+
 }
 
+function setPixelSize1(widthValue, heightValue, squareSideSize) 
+{   
+    let initial = squareSideSize;
+    while(widthValue % initial  != 0 && initial>10)
+        initial--;
 
+    if(initial % widthValue != 0)
+    {
+        initial = squareSideSize;
+        while(widthValue % initial != 0 && initial<700)
+        {
+            initial++;
+        }
+            
+    }
+    console.log(initial)
 
-function generateBoxes(width, height, squareSideSize)
+}
+
+function generateBoxes(widthValue, heightValue, squareSideSize)
 {
 
     let blocks = document.querySelectorAll('.test');
@@ -128,9 +203,7 @@ function generateBoxes(width, height, squareSideSize)
        container.removeChild(block);
     }
 
-    container.style.setProperty("max-width", `${width}px`);
-
-    for(let i = 0; i < (width/squareSideSize)*(height/squareSideSize); i++)
+    for(let i = 0; i < (widthValue/squareSideSize)*(heightValue/squareSideSize); i++)
     {
         let block = document.createElement('div');
         block.classList.add('test');
